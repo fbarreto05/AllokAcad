f85a2a50ccfb9ed0afbad4230e09b15bfbc0e09a
Fernando Barreto <fernandobarreto864@gmail.com>
2025-06-26 18:59:25 -0300

    Update login.js

--- AllokAcads/static/js/login.js
+++ AllokAcads/static/js/login.js
@@ -112,7 +112,7 @@ document.addEventListener('DOMContentLoaded', function() {
         let hasError = false;
         
         if (!emailInput.value.trim()) {
-            showError(identifierError, 'Por favor, insira seu e-mail');
+            showError(identifierError, 'Por favor, insira seu email');
             emailInput.classList.add('error');
             hasError = true;
         }
